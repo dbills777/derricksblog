e512a6abb709a5492443a1835689a754939c112b
Derrick Bills <dbills777@gmail.com>
2020-08-26 11:38:55 -0600

    remove pages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,105 +1,65 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from 'react';
+import { Link } from 'gatsby';
 
-import logo from '../img/logo.svg'
-import facebook from '../img/social/facebook.svg'
-import instagram from '../img/social/instagram.svg'
-import twitter from '../img/social/twitter.svg'
-import vimeo from '../img/social/vimeo.svg'
+import facebook from '../img/social/facebook.svg';
+import instagram from '../img/social/instagram.svg';
+import twitter from '../img/social/twitter.svg';
 
 const Footer = class extends React.Component {
   render() {
     return (
-      <footer className="footer has-background-black has-text-white-ter">
-        <div className="content has-text-centered">
-          <img
-            src={logo}
-            alt="Kaldi"
-            style={{ width: '14em', height: '10em' }}
-          />
-        </div>
-        <div className="content has-text-centered has-background-black has-text-white-ter">
-          <div className="container has-background-black has-text-white-ter">
-            <div style={{ maxWidth: '100vw' }} className="columns">
-              <div className="column is-4">
-                <section className="menu">
-                  <ul className="menu-list">
-                    <li>
-                      <Link to="/" className="navbar-item">
-                        Home
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/about">
-                        About
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/products">
-                        Products
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact/examples">
-                        Form Examples
-                      </Link>
-                    </li>
+      <footer className='footer has-background-black has-text-white-ter'>
+        <div className='content has-text-centered'></div>
+        <div className='content has-text-centered has-background-black has-text-white-ter'>
+          <div className='container has-background-black has-text-white-ter'>
+            <div style={{ maxWidth: '100vw' }} className='columns'>
+              <div className='column is-4'>
+                <section className='menu'>
+                  <ul className='menu-list'>
                     <li>
                       <a
-                        className="navbar-item"
-                        href="/admin/"
-                        target="_blank"
-                        rel="noopener noreferrer"
+                        className='navbar-item'
+                        href='/admin/'
+                        target='_blank'
+                        rel='noopener noreferrer'
                       >
-                        Admin
+                        CMS LOGIN
                       </a>
                     </li>
                   </ul>
                 </section>
               </div>
-              <div className="column is-4">
+              <div className='column is-4'>
                 <section>
-                  <ul className="menu-list">
+                  <ul className='menu-list'>
                     <li>
-                      <Link className="navbar-item" to="/blog">
-                        Latest Stories
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact">
-                        Contact
+                      <Link className='navbar-item' to='/blog'>
+                        BLOG POSTS
                       </Link>
                     </li>
                   </ul>
                 </section>
               </div>
-              <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
+              <div className='column is-4 social'>
+                <a title='facebook' href='https://facebook.com'>
                   <img
                     src={facebook}
-                    alt="Facebook"
+                    alt='Facebook'
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
-                <a title="twitter" href="https://twitter.com">
+                <a title='twitter' href='https://twitter.com'>
                   <img
-                    className="fas fa-lg"
+                    className='fas fa-lg'
                     src={twitter}
-                    alt="Twitter"
+                    alt='Twitter'
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
-                <a title="instagram" href="https://instagram.com">
+                <a title='instagram' href='https://instagram.com'>
                   <img
                     src={instagram}
-                    alt="Instagram"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="vimeo" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
+                    alt='Instagram'
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
@@ -108,8 +68,8 @@ const Footer = class extends React.Component {
           </div>
         </div>
       </footer>
-    )
+    );
   }
-}
+};
 
-export default Footer
+export default Footer;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,14 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import React from 'react';
+import { Link } from 'gatsby';
+import github from '../img/github-icon.svg';
 
 const Navbar = class extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       active: false,
       navBarActiveClass: '',
-    }
+    };
   }
 
   toggleHamburger = () => {
@@ -27,27 +26,24 @@ const Navbar = class extends React.Component {
             })
           : this.setState({
               navBarActiveClass: '',
-            })
+            });
       }
-    )
-  }
+    );
+  };
 
   render() {
     return (
       <nav
-        className="navbar is-transparent"
-        role="navigation"
-        aria-label="main-navigation"
+        className='navbar is-transparent'
+        role='navigation'
+        aria-label='main-navigation'
       >
-        <div className="container">
-          <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-            </Link>
+        <div className='container'>
+          <div className='navbar-brand'>
             {/* Hamburger menu */}
             <div
               className={`navbar-burger burger ${this.state.navBarActiveClass}`}
-              data-target="navMenu"
+              data-target='navMenu'
               onClick={() => this.toggleHamburger()}
             >
               <span />
@@ -56,43 +52,31 @@ const Navbar = class extends React.Component {
             </div>
           </div>
           <div
-            id="navMenu"
+            id='navMenu'
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
-            <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
-                About
-              </Link>
-              <Link className="navbar-item" to="/products">
-                Products
-              </Link>
-              <Link className="navbar-item" to="/blog">
+            <div className='navbar-start has-text-centered'>
+              <Link className='navbar-item' to='/blog'>
                 Blog
               </Link>
-              <Link className="navbar-item" to="/contact">
-                Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-              </Link>
             </div>
-            <div className="navbar-end has-text-centered">
+            <div className='navbar-end has-text-centered'>
               <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
+                className='navbar-item'
+                href='https://github.com/netlify-templates/gatsby-starter-netlify-cms'
+                target='_blank'
+                rel='noopener noreferrer'
               >
-                <span className="icon">
-                  <img src={github} alt="Github" />
+                <span className='icon'>
+                  <img src={github} alt='Github' />
                 </span>
               </a>
             </div>
           </div>
         </div>
       </nav>
-    )
+    );
   }
-}
+};
 
-export default Navbar
+export default Navbar;
